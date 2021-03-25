59351d6d76af6209448ca73db44d3972be0d5202
simoberny <simone.bernabe@studenti.unitn.it>
2021-03-26 00:05:31 +0100

    Check status on heywhats

--- app/src/main/java/it/bobbyfriends/howsthere/Hwt.java
+++ app/src/main/java/it/bobbyfriends/howsthere/Hwt.java
@@ -3,7 +3,10 @@ package it.bobbyfriends.howsthere;
 import android.content.Context;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.fragment.app.DialogFragment;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
@@ -12,6 +15,7 @@ import java.io.IOException;
 import java.util.Date;
 
 import it.bobbyfriends.howsthere.objects.Panorama;
+import it.bobbyfriends.howsthere.ui.home.HomeFragment;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -24,8 +28,13 @@ public class Hwt {
     private Context activity_context;
     private Retrofit retrofit = null;
     private BottomSheetDialog dialog;
+
     private TextView dialog_message;
+    private Button bRetry;
+
     public Panorama panorama = null;
+    private Integer max_retry = 2;
+    private Integer process_state = 0;
 
     public interface HeyWhatsID {
         @GET("api/query?src=hows")
@@ -50,13 +59,21 @@ public class Hwt {
     public Hwt (Context context) {
         this.activity_context = context;
         retrofit = new Retrofit.Builder()
-                .baseUrl("http://www.heywhatsthat.com/")
+                .baseUrl("https://www.heywhatsthat.com/")
                 .build();
 
         this.dialog = new BottomSheetDialog(activity_context);
         View dialogView = View.inflate(activity_context, R.layout.activity_loading, null);
         this.dialog.setCancelable(false);
         this.dialog.setContentView(dialogView);
+
+        this.bRetry = dialogView.findViewById(R.id.retry);
+        bRetry.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                retryStage();
+            }
+        });
 
         this.dialog_message = dialogView.findViewById(R.id.stat_news);
 
@@ -70,23 +87,31 @@ public class Hwt {
     }
 
     public void requestData(){
+        dialog_message.setText(activity_context.getResources().getString(R.string.get_id));
         dialog.show();
-        obtainId();
+        obtainId(0);
     }
 
-    public String obtainId(){
-        this.dialog_message.setText(activity_context.getResources().getString(R.string.get_id));
+    public void obtainId(Integer retry){
+        System.out.println("Retry: " + retry);
+
+        if(retry > max_retry){
+            bRetry.setVisibility(View.VISIBLE);
+            return;
+        }
 
         HeyWhatsID service = retrofit.create(HeyWhatsID.class);
         Call<ResponseBody> call = service.getID(this.panorama.lat, this.panorama.lon);
-        String id = "";
 
         call.enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                 if (response.isSuccessful()) {
+                    dialog_message.setText("Response susseccful");
                     try {
                         String id = response.body().string();
+
+                        dialog_message.setText("ID: " + id);
 
                         if(id != null && id != "") {
                             panorama.ID = id;
@@ -94,24 +119,41 @@ public class Hwt {
                             new Handler().postDelayed(new Runnable() {
                                 @Override
                                 public void run() {
-                                    checkStatus(); //Ottenuto l'ID controllo lo stato della generazione del panorama
+                                    checkStatus(id);
                                 }
-                            }, 500);
-                        }else{
-                            progressDialog.dismiss();
+                            }, 750);
                         }
                     } catch (IOException e) { e.printStackTrace(); }
-                } else {
-
                 }
+
+                //waitRetry(retry);
             }
+
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t)
             {
-
+                dialog_message.setText("Errore: " + t.getMessage());
+                //waitRetry(retry);
             }
         });
+    }
 
-        return id;
+    private void waitRetry(Integer retry){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                dialog_message.setText(activity_context.getResources().getString(R.string.failed_try));
+                obtainId(retry + 1);
+            }
+        }, 2000);
+    }
+
+    public void checkStatus(String id){
+        dialog_message.setText(activity_context.getResources().getString(R.string.get_status));
+        System.out.println("ID: " + id);
+    }
+
+    public void retryStage(){
+        System.out.println("Clicked!");
     }
 }
